e95584214a786da6a42911fdfb6a86793e798bdc
Diego Rodriguez <69642649+Diegoo3210@users.noreply.github.com>
2022-04-06 21:34:01 -0500

    Add files via upload

---
+++ sprint2Escalabilidad/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Sprint2EscalabilidadConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'sprint2Escalabilidad'
---
+++ sprint2Escalabilidad/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from . import models
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+
+
+def index(request):
+    return render(request, 'formulario.html')
+
+def uploadFile(request):
+    if request.method == "POST":
+        # Fetching the form data
+        fileTitle = request.POST["fileTitle"]
+        uploadedFile = request.FILES["uploadedFile"]
+
+        # Saving the information in the database
+        document = models.Document(
+            title = fileTitle,
+            uploadedFile = uploadedFile
+        )
+        document.save()
+
+    documents = models.Document.objects.all()
+
+    return render(request, "formulario.html", context = {
+        "files": documents
+    })
